eba89cecede08ed5c4eeac54c07c0905a626bef2
Bruno Soares <soares_bruno@sicredi.com.br>
2020-07-14 20:38:07 -0300

    ajusta o layout do money

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,22 @@ class Header extends React.Component {
             <div className="header">
                 <h1>Hoard Level</h1>
                 <div className="header-cards-container">
-                    <div className="card clickable" onClick={(e) => this.props.onButtonClick(0)}>0-4</div>
-                    <div className="card clickable" onClick={(e) => this.props.onButtonClick(5)}>5-10</div>
-                    <div className="card clickable" onClick={(e) => this.props.onButtonClick(11)}>11-16</div>
-                    <div className="card clickable" onClick={(e) => this.props.onButtonClick(17)}>17+</div>
+                    <div className="item card clickable" 
+                        onClick={(e) => this.props.onButtonClick(0)}>
+                        <div className="card-content">0-4</div>
+                    </div>
+                    <div className="item card clickable" 
+                        onClick={(e) => this.props.onButtonClick(5)}>
+                        <div className="card-content">5-10</div>
+                    </div>
+                    <div className="item card clickable" 
+                        onClick={(e) => this.props.onButtonClick(11)}>
+                        <div className="card-content">11-16</div>
+                    </div>
+                    <div className="item card clickable" 
+                        onClick={(e) => this.props.onButtonClick(17)}>
+                        <div className="card-content">17+</div>
+                    </div>
                 </div>
             </div>
         );
--- src/components/MoneyTable.js
+++ src/components/MoneyTable.js
@@ -4,13 +4,14 @@ import TreasureHoard from '../treasure-generator/TreasureHoard';
 class MoneyTable extends React.Component {
     
     render() {
+        let titles = ['Copper Pieces', 'Silver Pieces', 'Gold Pieces', 'Platinum Pieces']
         var level = this.props.hoardLevel;
         var hoard = new TreasureHoard();
         var money = hoard.money(level);
         var coins = [];
 
         for (var m in money) {
-            coins.push(<td key={m}>{money[m]}</td>);
+            coins.push(money[m]);
         }
 
         var display='none';
@@ -19,25 +20,40 @@ class MoneyTable extends React.Component {
         }
         
         return(
-            <div className="card" style={{display:`${display}`}}>
-                <div className="ui yellow header">Money</div>
-                <table className="ui yellow table">
-                    <thead>
-                        <tr>
-                            <th>Copper Pieces</th>
-                            <th>Silver Pieces</th>
-                            <th>Gold Pieces</th>
-                            <th>Platinum Pieces</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="warning">
-                            {coins}
-                        </tr>
-                    </tbody>
-                </table>
+            <div className="money-table-content card">
+                <div className="card-title">Money</div>
+                <div className="money-table-content-items card-content">
+                    {
+                        coins.map((coin, i) => {     
+                            return (
+                                <div key={i} className="money-table-content-item card">
+                                    <div className="card-title">{titles[i]}</div>
+                                    <div className="money-table-content-item-content card-content">
+                                        {coin}
+                                    </div>
+                                </div> 
+                            )
+                        })
+                    }
+                </div>
             </div>
         );
+            // <div className="card" style={{display:`${display}`}}>
+            //     <div className="ui yellow header">Money</div>
+            //     <table className="ui yellow table">
+            //         <thead>
+            //             <tr>
+            //                 <th></th>
+            //                 <th></th>
+            //             </tr>
+            //         </thead>
+            //         <tbody>
+            //             <tr className="warning">
+            //                 {coins}
+            //             </tr>
+            //         </tbody>
+            //     </table>
+            // </div>
     };
 }
 
